e56287e1ec3e8f3f02745c0473d6ad90a8eb1a8c
Luizzzt <fernanodluiz@gmail.com>
2026-09-20 18:05:27 -0300

    Implement SDL initialization and image loading
    Added SDL initialization and image loading functionality.

--- src/main.c
+++ src/main.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+
+#include "image.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(
+            stderr,
+            "Uso: %s caminho_da_imagem.ext\n",
+            argv[0]
+        );
+
+        return EXIT_FAILURE;
+    }
+
+    if (!SDL_Init(0))
+    {
+        fprintf(
+            stderr,
+            "Erro ao inicializar a SDL: %s\n",
+            SDL_GetError()
+        );
+
+        return EXIT_FAILURE;
+    }
+
+    Image image = {
+        .surface = NULL
+    };
+
+    if (!image_load(argv[1], &image))
+    {
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    image_destroy(&image);
+
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
