0dd7d35602439825be7af19c649684c50ca9136a
MauricioCustodioVicentini <mauriciocustodiovicentini@gmail.com>
2026-09-20 19:42:48 -0300

    Add window header file with SDL window management

--- src/window.h
+++ src/window.h
@@ -1,1 +1,33 @@
 
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <stdbool.h>
+#include <SDL3/SDL.h>
+
+enum
+{
+    MAIN_WINDOW_WIDTH = 1024,
+    MAIN_WINDOW_HEIGHT = 768
+};
+
+typedef struct
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *image_texture;
+    SDL_FRect image_rect;
+} AppWindow;
+
+bool window_initialize(AppWindow *app_window);
+
+bool window_set_image(
+    AppWindow *app_window,
+    SDL_Surface *surface
+);
+
+void window_render(AppWindow *app_window);
+
+void window_destroy(AppWindow *app_window);
+
+#endif
