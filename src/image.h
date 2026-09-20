3d510eefd580b1362539ffea0cda6b6760c55fd5
Luizzzt <fernanodluiz@gmail.com>
2026-09-20 17:34:05 -0300

    Add image.h header for image handling functions

--- src/image.h
+++ src/image.h
@@ -1,1 +1,15 @@
+#ifndef IMAGE_H
+#define IMAGE_H
 
+#include <stdbool.h>
+#include <SDL3/SDL.h>
+
+typedef struct
+{
+    SDL_Surface *surface;
+} Image;
+
+bool image_load(const char *filename, Image *image);
+void image_destroy(Image *image);
+
+#endif
