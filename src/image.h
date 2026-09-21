56293d09d904865727fd0d8c9c7cb8168f08e8e9
Luizzzt <fernanodluiz@gmail.com>
2026-09-21 19:47:59 -0300

    Refactor image.h for improved function declarations

--- src/image.h
+++ src/image.h
@@ -7,14 +7,44 @@
 typedef struct
 {
     SDL_Surface *surface;
+
+    /*
+     * Copia preservada da imagem original
+     * em escala de cinza.
+     *
+     * Sera usada posteriormente para
+     * retornar a imagem ao estado original
+     * sem recarregar o arquivo.
+     */
+    SDL_Surface *original_surface;
+
+    bool equalized;
+
 } Image;
 
-bool image_load(const char *filename, Image *image);
+bool image_load(
+    const char *filename,
+    Image *image
+);
 
-bool image_is_grayscale(const Image *image);
+bool image_is_grayscale(
+    const Image *image
+);
 
-bool image_convert_to_grayscale(Image *image);
+bool image_convert_to_grayscale(
+    Image *image
+);
 
-void image_destroy(Image *image);
+bool image_preserve_original(
+    Image *image
+);
+
+bool image_equalize(
+    Image *image
+);
+
+void image_destroy(
+    Image *image
+);
 
 #endif
