e7c20e8c8f4f46e20607009878779ed48e0fe867
Victor Sung Woo Hong <10425852@mackenzista.com.br>
2026-09-20 23:33:31 -0300

    Add brightness and contrast classification functions
    Added functions for brightness and contrast classification.

--- src/histogram.h
+++ src/histogram.h
@@ -9,13 +9,26 @@
 typedef struct
 {
     Uint64 bins[HISTOGRAM_LEVELS];
+
     Uint64 total_pixels;
     Uint64 max_count;
+
+    double mean;
+    double standard_deviation;
+
 } Histogram;
 
 bool histogram_calculate(
     Histogram *histogram,
     SDL_Surface *surface
+);
+
+const char *histogram_brightness_classification(
+    const Histogram *histogram
+);
+
+const char *histogram_contrast_classification(
+    const Histogram *histogram
 );
 
 #endif
