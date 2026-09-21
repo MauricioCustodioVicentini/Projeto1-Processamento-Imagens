a3e2e4c42c75cdc78ef64ac21858a9964bd569d1
MauricioCustodioVicentini <mauriciocustodiovicentini@gmail.com>
2026-09-21 00:22:04 -0300

    Update window.h

--- src/window.h
+++ src/window.h
@@ -1,4 +1,3 @@
-
 #ifndef WINDOW_H
 #define WINDOW_H
 
@@ -8,7 +7,10 @@
 enum
 {
     MAIN_WINDOW_WIDTH = 1024,
-    MAIN_WINDOW_HEIGHT = 768
+    MAIN_WINDOW_HEIGHT = 768,
+
+    INFO_WINDOW_WIDTH = 420,
+    INFO_WINDOW_HEIGHT = 650
 };
 
 typedef struct
@@ -17,7 +19,15 @@ typedef struct
     SDL_Renderer *renderer;
     SDL_Texture *image_texture;
     SDL_FRect image_rect;
+    SDL_WindowID id;
 } AppWindow;
+
+typedef struct
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_WindowID id;
+} InfoWindow;
 
 bool window_initialize(AppWindow *app_window);
 
@@ -29,5 +39,14 @@ bool window_set_image(
 void window_render(AppWindow *app_window);
 
 void window_destroy(AppWindow *app_window);
+
+bool info_window_initialize(
+    InfoWindow *info_window,
+    SDL_Window *parent_window
+);
+
+void info_window_render(InfoWindow *info_window);
+
+void info_window_destroy(InfoWindow *info_window);
 
 #endif
