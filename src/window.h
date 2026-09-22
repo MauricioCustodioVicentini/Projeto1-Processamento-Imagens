1cb60908aecd80c0873bf44d873dd5a067c61a39
Victor Sung Woo Hong <10425852@mackenzista.com.br>
2026-09-22 15:10:08 -0300

    Update window.h

--- src/window.h
+++ src/window.h
@@ -2,7 +2,9 @@
 #define WINDOW_H
 
 #include <stdbool.h>
+
 #include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
 #include "histogram.h"
 
@@ -40,7 +42,9 @@ typedef struct
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *image_texture;
+
     SDL_FRect image_rect;
+
     SDL_WindowID id;
 
     bool original_resolution;
@@ -51,10 +55,15 @@ typedef struct
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
+
     SDL_WindowID id;
 
     Button equalize_button;
     Button resolution_button;
+
+    TTF_Font *font;
+
+    bool ttf_initialized;
 
 } InfoWindow;
 
@@ -85,14 +94,16 @@ bool info_window_initialize(
     SDL_Window *parent_window
 );
 
-void info_window_render(
-    InfoWindow *info_window,
-    const Histogram *histogram
-);
-
 InfoAction info_window_handle_event(
     InfoWindow *info_window,
     const SDL_Event *event
+);
+
+void info_window_render(
+    InfoWindow *info_window,
+    const Histogram *histogram,
+    bool image_equalized,
+    bool original_resolution
 );
 
 void info_window_destroy(
